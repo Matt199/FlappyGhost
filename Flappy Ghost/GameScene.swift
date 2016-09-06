4b24c9cbab2ab505f7f0487b531f7cd98996948e
Matt <Matt@macbook-air-magda.local>
2016-09-06 16:08:39 +0200

    Added Gravity, and Physics

--- Flappy Ghost/GameScene.swift
+++ Flappy Ghost/GameScene.swift
@@ -8,6 +8,21 @@
 
 import SpriteKit
 
+
+
+
+
+struct PhysicsCategory {
+    static let Ghost : UInt32 = 0x1 << 1
+    static let Ground : UInt32 = 0x1 << 2
+    static let Wall : UInt32 = 0x1 << 3
+
+
+}
+
+
+
+
 class GameScene: SKScene {
     
     var ground = SKSpriteNode()
@@ -19,9 +34,26 @@ class GameScene: SKScene {
         ground.setScale(0.5)
         ground.position = CGPoint(x: self.frame.width/2, y: 0+ground.frame.height/2)
         
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.size)
+        ground.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        ground.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        ground.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        
+        ground.physicsBody?.affectedByGravity = false
+        ground.physicsBody?.dynamic = false
+        
+        
         ghost = SKSpriteNode(imageNamed: "Ghost")
         ghost.size = CGSize(width: 60, height: 70)
-        ghost.position = CGPoint(x: self.frame.width/2 - ghost.frame.width/2, y: self.frame.height/2)
+        ghost.position = CGPoint(x: self.frame.width/2 - ghost.frame.width, y: self.frame.height/2)
+        
+        
+        ghost.physicsBody = SKPhysicsBody(circleOfRadius: ghost.frame.width/2)
+        ghost.physicsBody?.collisionBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        ghost.physicsBody?.contactTestBitMask = PhysicsCategory.Ground | PhysicsCategory.Wall
+        ghost.physicsBody?.affectedByGravity = true
+        ghost.physicsBody?.dynamic = true
+        
         
         
         self.addChild(ghost)
@@ -33,7 +65,8 @@ class GameScene: SKScene {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
+        ghost.physicsBody?.velocity = CGVectorMake(10, 0)
+        ghost.physicsBody?.applyImpulse(CGVectorMake(0, 90))
        
         
     }
@@ -50,14 +83,32 @@ class GameScene: SKScene {
         topWall.setScale(0.5)
         btmWall.setScale(0.5)
         
-        topWall.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2+350)
-        btmWall.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2-350)
+        topWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2+350)
+        btmWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2-350)
 
+        topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.frame.size)
+        btmWall.physicsBody = SKPhysicsBody(rectangleOfSize: btmWall.frame.size)
+        
+        
+        topWall.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        topWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        topWall.physicsBody?.affectedByGravity = false
+        topWall.physicsBody?.dynamic = false
+        
+        
+        btmWall.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        btmWall.physicsBody?.collisionBitMask = PhysicsCategory.Ghost
+        btmWall.physicsBody?.contactTestBitMask = PhysicsCategory.Ghost
+        btmWall.physicsBody?.affectedByGravity = false
+        btmWall.physicsBody?.dynamic = false
+        
+        
         addWall.addChild(topWall)
         addWall.addChild(btmWall)
         
         topWall.zRotation = CGFloat(M_PI)
-        
+        btmWall.zPosition = 1
         self.addChild(addWall)
         
         
